81e4f13f34abec9cbb546fa90de00206e81b7633
TaniachiFractal <tnmslv@yandex.ru>
2024-09-21 19:06:35 +0300

    Add images and project files

---
+++ Maslova_task2_Snowfall/SnowfallForm.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace Maslova_task2_Snowfall
+{
+    partial class SnowfallForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SnowfallForm));
+            this.SuspendLayout();
+            // 
+            // SnowfallForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Maslova_task2_Snowfall.Properties.Resources.snowforest;
+            this.ClientSize = new System.Drawing.Size(640, 466);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SnowfallForm";
+            this.Text = "Снегопад";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
