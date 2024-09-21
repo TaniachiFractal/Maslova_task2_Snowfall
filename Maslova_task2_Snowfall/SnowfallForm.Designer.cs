1c1df5b33425933876ed19aedf2cc0b7da7321c6
TaniachiFractal <tnmslv@yandex.ru>
2024-09-21 21:32:48 +0300

    Finish animation

--- Maslova_task2_Snowfall/SnowfallForm.Designer.cs
+++ Maslova_task2_Snowfall/SnowfallForm.Designer.cs
@@ -29,25 +29,40 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SnowfallForm));
+            this.pbCanvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCanvas)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pbCanvas
+            // 
+            this.pbCanvas.BackColor = System.Drawing.Color.Transparent;
+            this.pbCanvas.Location = new System.Drawing.Point(0, 0);
+            this.pbCanvas.Name = "pbCanvas";
+            this.pbCanvas.Size = new System.Drawing.Size(640, 480);
+            this.pbCanvas.TabIndex = 0;
+            this.pbCanvas.TabStop = false;
             // 
             // SnowfallForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Maslova_task2_Snowfall.Properties.Resources.snowforest;
-            this.ClientSize = new System.Drawing.Size(640, 466);
+            this.ClientSize = new System.Drawing.Size(640, 480);
+            this.Controls.Add(this.pbCanvas);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "SnowfallForm";
             this.Text = "Снегопад";
+            ((System.ComponentModel.ISupportInitialize)(this.pbCanvas)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbCanvas;
     }
 }
 
--- Maslova_task2_Snowfall/SnowfallForm.cs
+++ Maslova_task2_Snowfall/SnowfallForm.cs
@@ -1,19 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
+using Maslova_task2_Snowfall.Classes;
 
 namespace Maslova_task2_Snowfall
 {
+    /// <summary>
+    /// The main animation form
+    /// Основная форма анимации
+    /// </summary>
     public partial class SnowfallForm : Form
     {
+        /// <summary>
+        /// The main drawing canvas
+        /// Основное полe для рисования
+        /// </summary>
+        private readonly Graphics canvas;
+
+        /// <summary>
+        /// Base constructor: Init timer, Init snowflakes
+        /// Базовый конструктор: инициализация таймера, снежинок
+        /// </summary>
         public SnowfallForm()
         {
             InitializeComponent();
+
+            canvas = pbCanvas.CreateGraphics();
+
+            Snowflake.Snowflakes(50, Height, Width);
+
+            var animTimer = new Timer
+            {
+                Interval = 30
+            };
+            animTimer.Tick += AnimTimer_Tick;
+            animTimer.Start();
+        }
+
+        int fieldRotation = 0;
+        bool rotateClockWise = false;
+        /// <summary>
+        /// Animate snowflakes
+        /// Анимировать снежинки
+        /// </summary>
+        private void AnimTimer_Tick(object sender, EventArgs e)
+        {
+            var newImage = new Bitmap(pbCanvas.Width, pbCanvas.Height);
+            var newGraphics = Graphics.FromImage(newImage);
+            newGraphics.DrawImage(Snowflake.SnowforestImg, 0, 0);
+            foreach (var snowflake in Snowflake.Snowflakes())
+            {
+                snowflake.MoveSnowflake(Height);
+                newGraphics.RotateTransform(snowflake.flyRotation);
+                newGraphics.DrawImage(Snowflake.SnowflakeImg, new Rectangle(snowflake.X, snowflake.Y, snowflake.size, snowflake.size));
+            }
+            canvas.DrawImage(newImage, 0, 0);
         }
     }
 }
